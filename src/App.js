3bf378896bf5a88e5fe364fe32acccb1b00e15c3
asadbek-dev <asadbek.dev1101@gmail.com>
2020-12-23 13:45:00 +0500

    all files changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ToDoList from './component/ToDoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
+//
---
+++ src/component/ToDoForm.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+function ToDoForm(props) {
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        inputRef.current.focus()
+    })
+
+
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 1000),
+            text: input
+        });
+
+
+        setInput('');
+    }
+    return (
+        <form className="todo-form" onSubmit={handleSubmit}>
+            <input type="text" onChange={handleChange} placeholder="Add to do" value={input} name='text' className='todo-input' ref={inputRef} />
+            <button className='todo-button'>Add todo</button>
+        </form>
+    )
+}
+
+export default ToDoForm
